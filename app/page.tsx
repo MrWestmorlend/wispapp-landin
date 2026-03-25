a26e8701f3e4e300be2bc6daca34a5be20127cd9
root <root@ucazjyfodo.local>
2026-03-25 17:22:58 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 import Header from "./components/header/index";
 import Hero from "./components/hero/index";
 import PhoneMockup from "./components/phonemockup/index";
-import PostsGrid from "./components/postsgrid/index";
 import Footer from "./components/footer/index";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#0a0a0f]">
+    <div className="min-h-screen bg-[#dcfce7]">
       <Header />
       <Hero />
       <PhoneMockup />
-      <PostsGrid />
       <Footer />
     </div>
   );
